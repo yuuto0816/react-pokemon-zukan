1e66652464a950ec390f2123fbe1615cb1dbb4a1
yuuto0816 <ktc24a31g0019@edu.kyoto-tech.ac.jp>
2024-10-12 12:39:25 +0900

    変更

--- src/pages/PokemonDetail.tsx
+++ src/pages/PokemonDetail.tsx
@@ -1,11 +1,11 @@
 const PokemonDetail: React.FC = () => {
     return (
-        <div className="text-center mt-10 bg-slate-500">
-            <h1 className="text-4xl font-bold text-blue-500">Reactポケモン図鑑詳細</h1>
-            <div className="bg-slate-500">
-                ここにポケモンの画像やステータスなどを表示する。
-            </div>
+      <div className="text-center mt-10 bg-slate-500">
+        <h1 className="text-4xl font-bold text-blue-500">Reactポケモン図鑑詳細</h1>
+        <div className="bg-slate-500">
+          ここにポケモンの画像やステータスなどを表示する。
         </div>
+      </div>
     )
 }
 
