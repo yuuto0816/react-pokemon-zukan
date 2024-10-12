a7c89ae1358ea4ed0c744a7c422655939cb85805
yuuto0816 <ktc24a31g0019@edu.kyoto-tech.ac.jp>
2024-10-12 12:06:44 +0900

    変更

---
+++ src/pages/PokemonDetail.tsx
@@ -0,0 +1,12 @@
+const PokemonDetail: React.FC = () => {
+    return (
+        <div className="text-center mt-10 bg-slate-500">
+            <h1 className="text-4xl font-bold text-blue-500">Reactポケモン図鑑詳細</h1>
+            <div className="bg-slate-500">
+                ここにポケモンの画像やステータスなどを表示する。
+            </div>
+        </div>
+    )
+}
+
+export default PokemonDetail;
---
+++ src/pages/PokemonList.tsx
@@ -0,0 +1,9 @@
+const PokemonList: React.FC = () => {
+    return (
+        <div className="text-center mt-10">
+            <h1 className="text-4xl font-bold text-blue-500">Reactポケモン図鑑リスト</h1>
+        </div>
+    )
+}
+
+export default PokemonList;
