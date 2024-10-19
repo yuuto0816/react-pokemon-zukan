a6e4ca52859efa3b25d4d4b99187536e26370fff
yuuto0816 <ktc24a31g0019@edu.kyoto-tech.ac.jp>
2024-10-19 10:33:04 +0900

    hennkou

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import PokemonList from './pages/PokemonList';
-import PokemonDetail from './pages/PokemonList';
+import PokemonDetail from './pages/PokemonDetail';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<PokemonList />} />
-      <Route path="/pokemon/:id" element={<PokemonDetail />} />
-    </Routes>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <Navigation />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<PokemonList />} />
+          <Route path="/pokemon/:id" element={<PokemonDetail />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
--- src/api/common.type.ts
+++ src/api/common.type.ts
@@ -1,15 +1,15 @@
 // src/api/common.type.ts
 
 export interface NamedAPIResource {
-    name: string;
-    url: string;
-  }
-  
-  export interface Name {
-    name: string;
-    language: NamedAPIResource;
-  }
-  
-  export interface APIResource {
-    url: string;
-  }
+  name: string;
+  url: string;
+}
+
+export interface Name {
+  name: string;
+  language: NamedAPIResource;
+}
+
+export interface APIResource {
+  url: string;
+}
