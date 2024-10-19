52d6a97bd0989e1c05fc6417704c831e304f0df5
yuuto0816 <ktc24a31g0019@edu.kyoto-tech.ac.jp>
2024-10-19 09:53:58 +0900

    Set up Vite React TypeScript project

--- src/pages/PokemonDetail.tsx
+++ src/pages/PokemonDetail.tsx
@@ -1,4 +1,5 @@
 const PokemonDetail: React.FC = () => {
+  const PokemonDetail: React.FC = () => {
     return (
       <div className="text-center mt-10 bg-slate-500">
         <h1 className="text-4xl font-bold text-blue-500">Reactポケモン図鑑詳細</h1>
