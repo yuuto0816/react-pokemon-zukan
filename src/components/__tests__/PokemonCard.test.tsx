8023719b18a0b9dc58bdbc65de5407873fa4152a
yuuto0816 <ktc24a31g0019@edu.kyoto-tech.ac.jp>
2024-10-19 11:01:26 +0900

    henkou

---
+++ src/components/__tests__/PokemonCard.test.tsx
@@ -0,0 +1,22 @@
+// src/components/__tests__/PokemonCard.test.tsx
+import React from 'react';
+import { render } from '@testing-library/react';
+import PokemonCard from '../PokemonCard';
+import { BrowserRouter } from 'react-router-dom';
+
+test('ポケモンカードが正しくレンダリングされる', () => {
+  const pokemon = {
+    name: 'bulbasaur',
+    url: '<https://pokeapi.co/api/v2/pokemon/1/>',
+    japaneseName: 'フシギダネ',
+  };
+
+  const { getByText, getByAltText } = render(
+    <BrowserRouter>
+      <PokemonCard pokemon={pokemon} />
+    </BrowserRouter>
+  );
+
+  expect(getByText('フシギダネ')).toBeInTheDocument();
+  expect(getByAltText('フシギダネ')).toBeInTheDocument();
+});
---
+++ src/components/__tests__/jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+module.exports = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+      '\\\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+  };
+  
