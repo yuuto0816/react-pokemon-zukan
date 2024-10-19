64f050c2449ac5ea73d3b56db3cb8c1616b8ba51
yuuto0816 <ktc24a31g0019@edu.kyoto-tech.ac.jp>
2024-10-19 11:55:34 +0900

    15. テストとデバッグの仕方（削除）

--- src/components/__tests__/jest.config.js
+++
@@ -1,9 +0,0 @@
-// jest.config.js
-module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    moduleNameMapper: {
-      '\\\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    },
-  };
-  
