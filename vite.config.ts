466401aa7cefc76b2dea65e4997900d65007d8d7
yuuto0816 <ktc24a31g0019@edu.kyoto-tech.ac.jp>
2024-10-19 10:43:29 +0900

    henkou

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+  base: command === 'serve' ? '/' : '/react-pokemon-zukan/',
+}));
